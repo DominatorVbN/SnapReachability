fa54c5807937a8a388a89e4f84c75f183e3b36c8
AmitDevBol <-list>
2020-08-14 01:33:38 +0530

    Added target

---
+++ Example/SnapRechability/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapRechability
+//
+//  Created by dominator on 14/08/20.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
